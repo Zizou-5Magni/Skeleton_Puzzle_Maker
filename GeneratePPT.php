d88c5e157b76ec306141af8dd2a125a7da6fd3e0
Zizo the Great <Titomoe89@yahoo.com>
2021-11-09 12:26:01 -0600

    PPT edit on generation page

--- GeneratePPT.php
+++ GeneratePPT.php
@@ -7,6 +7,8 @@ use PhpOffice\PhpPresentation\IOFactory;
 use PhpOffice\PhpPresentation\PhpPresentation;
 use PhpOffice\PhpPresentation\Style\Alignment;
 use PhpOffice\PhpPresentation\Style\Color;
+use PhpOffice\PhpPresentation\Style\Border;
+use PhpOffice\PhpPresentation\Style\Fill;
 
 
 require_once 'libs/PhpPresentation/src/PhpPresentation/Autoloader.php';
@@ -18,10 +20,12 @@ require_once 'libs/Common/src/Common/Autoloader.php';
 
 
 $json = file_get_contents('php://input');
-$data = json_decode($json);
+$skeletons = json_decode($json);
 
-$skeletons = $data->skeletons;
-$solutions = $data->solutions;
+// $data = json_decode($json);
+
+// $skeletons = $data->skeletons;
+// $solutions = $data->solutions;
 
 $presentation = new PhpPresentation();
 
@@ -53,22 +57,44 @@ foreach ($skeletons as $i => $skeleton) {
         ->setColor(new Color('FF0D6EFD'));
 
     //Add Skeleton Image
-    $imageShape = new Base64();
-    $imageShape
-        ->setName('Skeleton')
-        ->setDescription('Skeleton')
-        ->setData($skeleton->image)
-        ->setResizeProportional(false)
-        ->setHeight($skeleton->height)
-        ->setWidth($skeleton->width)
-        ->setOffsetX((960 - $skeleton->width) / 2)
-        ->setOffsetY(200);
+    $rows = $skeleton->puzzle;
+    $rows_count = count($rows);
+    $columns_count = count(array_keys((array)$rows[0]));
 
-    $currentSlide->addShape($imageShape);
+    // Create a shape (table)
+    $shape = $currentSlide->createTableShape($columns_count); //number of columns
+    $shape->setHeight(50 * $rows_count);
+    $shape->setOffsetX((960 - $columns_count * 50) / 2);
+    $shape->setOffsetY(200);
+
+
+    foreach ($rows as $i => $row) {
+        // Add row
+        $tableRow = $shape->createRow();
+        $tableRow->setHeight(50);
+
+        foreach ($row as $k => $col) {
+            $oCell = $tableRow->nextCell();
+            $oCell->setWidth(50);
+            if ($col === 0) {
+
+                $oCell->getFill()->setFillType(Fill::FILL_SOLID)
+                    ->setStartColor(new Color('FF000000'))
+                    ->setEndColor(new Color('FF000000'));
+            }
+            $textRun = $oCell->createTextRun('');
+            $textRun->getFont()
+                ->setBold(true)
+                ->setSize(20);
+
+            $oCell->getActiveParagraph()
+                ->getAlignment()
+                ->setHorizontal(Alignment::HORIZONTAL_CENTER)
+                ->setVertical(Alignment::VERTICAL_CENTER);
+        }
+    }
 }
-
-
-foreach ($solutions as $i => $solution) {
+foreach ($skeletons as $i => $skeleton) {
     // Create slide
     $currentSlide = $presentation->createSlide();
 
@@ -80,6 +106,8 @@ foreach ($solutions as $i => $solution) {
         ->setOffsetX(10)
         ->setOffsetY(10);
     $textShape->getActiveParagraph()->getAlignment()->setHorizontal(Alignment::HORIZONTAL_LEFT);
+    //if inline to check if we have more than one skeleton, if so, get the index of the loop and add 1 
+    //(since loops start with index 0), else the variable number will hold the '' empty value
     $number = count($skeletons) > 1 ? $i + 1 : '';
     $textRun = $textShape->createTextRun('Solution ' . $number);
     $textRun->getFont()
@@ -87,21 +115,53 @@ foreach ($solutions as $i => $solution) {
         ->setSize(28)
         ->setColor(new Color('FF0D6EFD'));
 
-    //Add Skeleton Image
-    $imageShape = new Base64();
-    $imageShape
-        ->setName('Solution Skeleton')
-        ->setDescription('Solution Skeleton')
-        ->setData($solution->image)
-        ->setResizeProportional(false)
-        ->setHeight($solution->height)
-        ->setWidth($solution->width)
-        ->setOffsetX((960 - $solution->width) / 2)
-        ->setOffsetY(200);
-
-    $currentSlide->addShape($imageShape);
-}
-
+        $rows = $skeleton->solution;
+        $rows_count = count($rows);
+        $columns_count = count(array_keys((array)$rows[0]));
+    
+        // Create a shape (table)
+        $shape = $currentSlide->createTableShape($columns_count); //number of columns
+        $shape->setHeight(50 * $rows_count);
+        $shape->setOffsetX((960 - $columns_count * 50) / 2);
+        $shape->setOffsetY(200);
+    
+    
+        foreach ($rows as $i => $row) {
+            // Add row
+            $tableRow = $shape->createRow();
+            $tableRow->setHeight(50);
+    
+            foreach ($row as $k => $col) {
+                $oCell = $tableRow->nextCell();
+                $oCell->setWidth(50);
+                if ($col === 0) {
+                    $oCell->getBorders()->getLeft()->setLineStyle(Border::LINE_NONE);
+                    $oCell->getBorders()->getTop()->setLineStyle(Border::LINE_NONE);
+                    $oCell->getBorders()->getRight()->setLineStyle(Border::LINE_NONE);
+                    $oCell->getBorders()->getBottom()->setLineStyle(Border::LINE_NONE);
+                    $oCell->getFill()->setFillType(Fill::FILL_SOLID)
+                        ->setStartColor(new Color('FF000000'))
+                        ->setEndColor(new Color('FF000000'));
+                } else {
+                    $oCell->getBorders()->getLeft()->setLineStyle(Border::LINE_SINGLE);
+                    $oCell->getBorders()->getTop()->setLineStyle(Border::LINE_SINGLE);
+                    $oCell->getBorders()->getRight()->setLineStyle(Border::LINE_SINGLE);
+                    $oCell->getBorders()->getBottom()->setLineStyle(Border::LINE_SINGLE);
+                    $oCell->getFill()->setFillType(Fill::FILL_SOLID)
+                        ->setStartColor(new Color('FFF5F5F5'))
+                        ->setEndColor(new Color('FFF5F5F5'));
+                    $textRun = $oCell->createTextRun($col);
+                    $textRun->getFont()
+                        ->setSize(20);
+    
+                    $oCell->getActiveParagraph()
+                        ->getAlignment()
+                        ->setHorizontal(Alignment::HORIZONTAL_CENTER)
+                        ->setVertical(Alignment::VERTICAL_CENTER);
+                }
+            }
+        }
+    }
 
 
 
